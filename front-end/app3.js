32bf8d27da9fb7b44a9f0c339e5e465b271379eb
Adam Oken <Adam_Oken@Adams-MacBook-Air.local>
2016-03-23 03:36:02 -0600

    renamed files and minor style change

--- front-end/app3.js
+++ front-end/app3.js
@@ -10,18 +10,7 @@ d3.json('https://pure-cove-57024.herokuapp.com/api/samples', function(err, data)
   })
   .style("background-color", "black")
   .on("click", function(d){
-	// d3.json("http://localhost:3000/api/sample/" + d.name)
- //    .header("Content-Type", "application/json")
- //    .get(JSON.stringify({name : d.name}), function(error, data) {
- //    	d3.select(".participant")
-	// 	  .append("a")
-	// 	  .attr("href", "index3.html?sample=" + data[0].name)
-	// 	  .html("new page");
- //    	console.log(error);
- //    	console.log(data)
- //    	console.log(data)
- //    })
- window.location.assign("index3.html?sample=" + d.name)
+ window.location.assign("geneprofile.html?sample=" + d.name)
 })
 })
 
